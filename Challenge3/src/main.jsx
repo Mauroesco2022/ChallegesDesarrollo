d9876cdc754f6baad29e730668c07199833b3501
Mauroesco2022 <mauricio.escobar@uao.edu.co>
2023-03-03 17:02:42 -0500

    Aqui mando el Challenge3

--- Challenge3/src/main.jsx
+++ Challenge3/src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+import FirstApp from './FirstApp'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FirstApp />
   </React.StrictMode>,
 )
