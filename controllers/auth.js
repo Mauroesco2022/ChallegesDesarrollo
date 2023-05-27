f7df1f510e52413eec060f09daa377b751817068
Mauroesco2022 <mauricio.escobar@uao.edu.co>
2023-05-26 22:30:45 -0500

    challenge18

--- controllers/auth.js
+++ controllers/auth.js
@@ -85,27 +85,7 @@ const revalidarToken = (req, res = express.request) => {
     })
 
 
-}
-// const listarUsuarios = async (req, res = express.request) => {
-//     const usuarios = await Usuario.find().populate('tareas', 'title');
-
-//     try{
-//         res.status(200).json({
-//             ok:true,
-//             usuarios,
-//         })
-//     } catch(error){
-//         console.log(error)
-//         res.status(500).json({
-//             ok: false,
-//             msg: 'Internal error',
-
-//         })
-
-//     }
-
-// }
-
+};
 module.exports = {
     loginUsuario,
     crearUsuario, 
